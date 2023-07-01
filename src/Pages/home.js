4a574039469ca04f26a37d388e214430a4060f4f
ankit yadav <ayadav@kloudrac.com>
2023-07-01 15:55:39 +0530

    top carousel added

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PopularItems from '../Components/Carousels/popularItems'
+import FeaturedItems from '../Components/Carousels/featuredItems'
+import FeaturedCategory from '../Components/Carousels/featuredCategory'
 export default function Home() {
   return (
     <>
-<PopularItems/>
-    <div style={{height:"800px"}}>Home Page</div>
+      <PopularItems/>
+      <FeaturedCategory/>
+      {/* <FeaturedItems/> */}
+    {/* <div style={{height:"800px"}}>Home Page</div> */}
     </>
 
   )

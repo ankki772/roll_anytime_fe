3d7701264ecc46f765212efbcc6d5ebe492a312e
diwediashish <adiwedi1@kloudrac.com>
2023-07-27 11:03:55 +0530

    changes

---
+++ src/Components/about.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import AboutImage from '../Assets/images/figma12 1.png'
+
+export default function About() {
+  return (
+    <>
+    <div className='about-content'>
+      <div className='first'>
+      <img src={AboutImage} alt='abou' className="img2" loading="lazy"/>
+      </div>
+      <div className='second'>
+        <h2>
+        About <br/>
+        Roll Anytime
+        </h2>
+        <h6>
+        Convenient access to any item
+        </h6>
+        <p>
+        Car Rental Experience was extraordinary going with this taxis administration rental. The rates were moderate, driver was acceptable.<br/>
+        Car Rental Experience was extraordinary going with this taxis administration rental. The rates were moderate, driver was acceptable.
+        </p>
+      </div>
+    </div>
+    
+    <style jsx >{`
+  .about-content{
+    // height:50%;
+    width:100%;
+    // background-color:red;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    gap:20px
+    padding: 20px 0px 20px 0px;
+
+  }
+  .first{
+    width: 50%;
+    display: flex;
+    height: 90%;
+    justify-content: center;
+    align-items: center;
+  }
+  .second{
+    width: 30%;
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    justify-content: center;
+  }
+  h2{
+    font-family: Inter;
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 58px;
+    letter-spacing: 0em;
+    text-align: left;
+    justify-content:left;
+
+  }
+  .img2{
+    height:90%;
+    width:90%;
+  }
+  .second h6{
+    font-family: Inter;
+    font-size: 20px;
+    font-style: italic;
+    font-weight: 500;
+    line-height: 31px;
+    letter-spacing: 0em;
+    text-align: left;
+
+  }
+  .second p{
+    font-family: Inter;
+    font-size: 16px;
+    font-style: italic;
+    font-weight: 400;
+    line-height: 25px;
+    letter-spacing: 0em;
+    text-align: left;
+
+  }
+  @media screen and (max-width: 768px) {
+    .about-content{
+      display: flex;
+      flex-direction: column-reverse;
+      padding: 10px 45px 10px 45px;
+      margin-top:20px;
+
+    }
+    .first{
+      width: 100%;
+      // height: 60%;
+      justify-content: flex-start;
+      padding-left:30px;
+    }
+    .second{
+      width: 100%;
+      // height: 40%;
+      padding-left:32px;
+    }
+  }
+  @media screen and (max-width: 425px) {
+    .about-content{
+      padding: 10px 10px 10px 10px;
+
+    }
+  }
+    `}
+    </style>
+    </>
+  )
+}
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import PopularItems from '../Components/Carousels/popularItems'
 import FeaturedItems from '../Components/Carousels/featuredItems'
 import FeaturedCategory from '../Components/Carousels/featuredCategory'
+import About from '../Components/about'
 export default function Home() {
   return (
     <>
       {/* <PopularItems/> */}
       <FeaturedCategory/>
+      <About/>
       {/* <FeaturedItems/> */}
     {/* <div style={{height:"800px"}}>Home Page</div> */}
     </>
